f19c8ef7a59d3ee5bf7a678dd19890bd2d331a1c
hermanlyx <hermanlyx@gmail.com>
2022-02-10 05:02:48 +0800

    deployment fixes 4

--- server.js
+++ server.js
@@ -25,7 +25,7 @@ mongoose.connection.once("open", () => {
 });
 
 //* MIDDLEWARE
-if (process) app.use(express.static(path.join(__dirname, "./client/build")));
+app.use(express.static(path.join(__dirname, "./client/build")));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
